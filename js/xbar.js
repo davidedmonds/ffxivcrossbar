6ae8ba7dd80c3618f6d18f88dba79ee38642145a
Kevin Phung <phung.kevin@gmail.com>
2013-10-10 01:08:52 -0700

    added pugilist, marauder and lancer skills

--- js/xbar.js
+++ js/xbar.js
@@ -51,7 +51,7 @@ jQuery(window).ready(function () {
         var clas = jQuery('#class').find('option:selected').attr('value'),
             raw_abilities = data.abilities,
             my_abilities = [],
-            keys, i, k, div, img, ability, url;
+            keys, i, k, div, img, ability, url, dir;
         
         keys = Object.keys(raw_abilities);
         
@@ -63,7 +63,14 @@ jQuery(window).ready(function () {
                 ability = raw_abilities[k];
                 my_abilities.push(ability);
                 
-                url = 'http://xivdbimg.zamimg.com/images/icons/000000/' + ability.icon + '.png';
+                if (Number(ability.icon) >= 3000) {
+                    dir = "003000";
+                }
+                else {
+                    dir = "000000";
+                }
+                
+                url = 'http://xivdbimg.zamimg.com/images/icons/' + dir + '/' + ability.icon + '.png';
                 
                 div = jQuery('<div/>')
                     .attr('class', 'skillContainer')
@@ -138,6 +145,7 @@ jQuery(window).ready(function () {
 
 data = {
     abilities : {
+        // Gladiator
         gla_fast_blade : {
             label : 'Fast Blade',
             icon : '000208',
@@ -222,6 +230,262 @@ data = {
             label : 'Circle of Scorn',
             icon : '000211',
             'class' : 'gladiator'
+        },
+        
+        // Pugilist
+        pgl_bootshine : {
+            label : 'Bootshine',
+            icon : '000308',
+            'class' : 'pugilist'
+        },
+        pgl_true_strike : {
+            label : 'True Strike',
+            icon : '000309',
+            'class' : 'pugilist'
+        },
+        pgl_snap_punch : {
+            label : 'Snap Punch',
+            icon : '000310',
+            'class' : 'pugilist'
+        },
+        pgl_second_wind : {
+            label : 'Second Wind',
+            icon : '000301',
+            'class' : 'pugilist'
+        },
+        pgl_haymaker : {
+            label : 'Haymaker',
+            icon : '000303',
+            'class' : 'pugilist'
+        },
+        pgl_twin_snakes : {
+            label : 'Twin Snakes',
+            icon : '000313',
+            'class' : 'pugilist'
+        },
+        pgl_internal_release : {
+            label : 'Internal Release',
+            icon : '000312',
+            'class' : 'pugilist'
+        },
+        pgl_touch_of_death : {
+            label : 'Touch of Death',
+            icon : '000311',
+            'class' : 'pugilist'
+        },
+        pgl_fists_of_earth : {
+            label : 'Fists of Earth',
+            icon : '000306',
+            'class' : 'pugilist'
+        },
+        pgl_arm_of_the_destroyer : {
+            label : 'Arm of the Destroyer',
+            icon : '000315',
+            'class' : 'pugilist'
+        },
+        pgl_demolish : {
+            label : 'Demolish',
+            icon : '000304',
+            'class' : 'pugilist'
+        },
+        pgl_fists_of_wind : {
+            label : 'Fists of Wind',
+            icon : '003102',
+            'class' : 'pugilist'
+        },
+        pgl_steel_peak : {
+            label : 'Steel Peak',
+            icon : '003105',
+            'class' : 'pugilist'
+        },
+        pgl_mantra : {
+            label : 'Mantra',
+            icon : '000316',
+            'class' : 'pugilist'
+        },
+        pgl_howling_fist : {
+            label : 'Howling Fist',
+            icon : '000307',
+            'class' : 'pugilist'
+        },
+        pgl_perfect_balance : {
+            label : 'Perfect Balance',
+            icon : '000317',
+            'class' : 'pugilist'
+        },
+        
+        // Marauder
+        mrd_heavy_swing : {
+            label : 'Heavy Swing',
+            icon : '000410',
+            'class' : 'marauder'
+        },
+        mrd_foresight : {
+            label : 'Foresight',
+            icon : '000402',
+            'class' : 'marauder'
+        },
+        mrd_skull_sunder : {
+            label : 'Skull Sunder',
+            icon : '000407',
+            'class' : 'marauder'
+        },
+        mrd_fracture : {
+            label : 'Fracture',
+            icon : '000406',
+            'class' : 'marauder'
+        },
+        mrd_bloodbath : {
+            label : 'Bloodbath',
+            icon : '000401',
+            'class' : 'marauder'
+        },
+        mrd_brutal_swing : {
+            label : 'Brutal Swing',
+            icon : '000403',
+            'class' : 'marauder'
+        },
+        mrd_overpower : {
+            label : 'Overpower',
+            icon : '000404',
+            'class' : 'marauder'
+        },
+        mrd_tomahawk : {
+            label : 'Tomahawk',
+            icon : '000411',
+            'class' : 'marauder'
+        },
+        mrd_maim : {
+            label : 'Maim',
+            icon : '000405',
+            'class' : 'marauder'
+        },
+        mrd_berserk : {
+            label : 'Berserk',
+            icon : '000409',
+            'class' : 'marauder'
+        },
+        mrd_mercy_stroke : {
+            label : 'Mercy Stroke',
+            icon : '000415',
+            'class' : 'marauder'
+        },
+        mrd_butchers_block : {
+            label : "Butcher's Block",
+            icon : '000412',
+            'class' : 'marauder'
+        },
+        mrd_thrill_of_battle : {
+            label : 'Thrill of Battle',
+            icon : '000413',
+            'class' : 'marauder'
+        },
+        mrd_storms_path : {
+            label : "Storm's Path",
+            icon : '000408',
+            'class' : 'marauder'
+        },
+        mrd_holmgang : {
+            label : 'Holmgang',
+            icon : '000416',
+            'class' : 'marauder'
+        },
+        mrd_vengeance : {
+            label : 'Vengeance',
+            icon : '000417',
+            'class' : 'marauder'
+        },
+        mrd_storms_eye : {
+            label : "Storm's Eye",
+            icon : '000414',
+            'class' : 'marauder'
+        },
+        
+        // Lancer
+        lnc_true_thrust : {
+            label : 'True Thrust',
+            icon : '000510',
+            'class' : 'lancer'
+        },
+        lnc_feint : {
+            label : 'Feint',
+            icon : '000507',
+            'class' : 'lancer'
+        },
+        lnc_vorpal_thurst : {
+            label : 'Vorpal Thrust',
+            icon : '000512',
+            'class' : 'lancer'
+        },
+        lnc_keen_flurry : {
+            label : 'Keen Flurry',
+            icon : '000501',
+            'class' : 'lancer'
+        },
+        lnc_impulse_drive : {
+            label : 'Impulse Drive',
+            icon : '000513',
+            'class' : 'lancer'
+        },
+        lnc_leg_sweep : {
+            label : 'Leg Sweep',
+            icon : '000505',
+            'class' : 'lancer'
+        },
+        lnc_heavy_thrust : {
+            label : 'Heavy Thrust',
+            icon : '000511',
+            'class' : 'lancer'
+        },
+        lnc_piercing_talon : {
+            label : 'Piercing Talon',
+            icon : '000515',
+            'class' : 'lancer'
+        },
+        lnc_life_surge : {
+            label : 'Life Surge',
+            icon : '000504',
+            'class' : 'lancer'
+        },
+        lnc_invigorate : {
+            label : 'Invigorate',
+            icon : '000502',
+            'class' : 'lancer'
+        },
+        lnc_full_thrust : {
+            label : 'Full Thrust',
+            icon : '000514',
+            'class' : 'lancer'
+        },
+        lnc_phlebotomize : {
+            label : 'Phlebotomize',
+            icon : '000516',
+            'class' : 'lancer'
+        },
+        lnc_blood_for_blood : {
+            label : 'Blood For Blood',
+            icon : '000509',
+            'class' : 'lancer'
+        },
+        lnc_disembowel : {
+            label : 'Disembowel',
+            icon : '000517',
+            'class' : 'lancer'
+        },
+        lnc_doom_spike : {
+            label : 'Doom Spike',
+            icon : '000506',
+            'class' : 'lancer'
+        },
+        lnc_ring_of_thorns : {
+            label : 'Ring of Thorns',
+            icon : '003304',
+            'class' : 'lancer'
+        },
+        lnc_chaos_thrust : {
+            label : 'Chaos Thrust',
+            icon : '000508',
+            'class' : 'lancer'
         }
     }
 }
